ad32cbee98b6d2a3092f6052c67172057fc163e9
lacamillaa <12999356@itisrossi.vi.it>
2026-02-19 14:12:11 +0100

    miglioramenti alla videocamera e alle dimensioni del labirinto

--- Assets/CPlayerCamera.cs
+++ Assets/CPlayerCamera.cs
@@ -2,38 +2,26 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public float sensX;  //senisibiltià della asse X
-    public float sensY;  //senisibiltià della asse Y
+    public float mouseSensitivity = 250;
 
-    public Transform orientation; //Orientamento 
+    float xRotation = 0;
+    float yRotation = 0;
 
-    float xRotazione; //Movimento di rotazione di X
-    float yRotazione; //Movimento di rotazione di Y
-
-
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Otteniamo la posizione dela X del cursore
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        //Otteniamo la posizione dela Y del cursore
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        //
-        yRotazione += mouseX;
-        xRotazione -= mouseY;
-        xRotazione = Mathf.Clamp(xRotazione, -90f, 90f);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        // Ruotiamo la camera
-        transform.rotation = Quaternion.Euler(xRotazione, yRotazione, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotazione, 0);
+        yRotation += mouseX;
 
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
     }
 }
